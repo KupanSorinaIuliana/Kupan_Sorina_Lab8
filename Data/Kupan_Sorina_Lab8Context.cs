b967e3ae6488a350a95e4020cfd80bf84f507e28
SORINA-IULIANA KUPAN <sorina.kupan@stud.ubbcluj.ro>
2020-11-24 22:08:15 +0200

    Add project files.

---
+++ Data/Kupan_Sorina_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Kupan_Sorina_Lab8.Models;
+
+namespace Kupan_Sorina_Lab8.Data
+{
+    public class Kupan_Sorina_Lab8Context : DbContext
+    {
+        public Kupan_Sorina_Lab8Context (DbContextOptions<Kupan_Sorina_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Kupan_Sorina_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Kupan_Sorina_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Kupan_Sorina_Lab8.Models.Category> Category { get; set; }
+    }
+}
